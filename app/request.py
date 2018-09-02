e3a92abc8dc8ae855ab0fb2c650c564966cd8752
Michel Atieno <mishqamish@gmail.com>
2018-09-02 12:54:57 +0300

    Added bootstrap into view

--- app/request.py
+++ app/request.py
@@ -1,15 +1,17 @@
 from app import app
 import urllib.request,json
-from .models import news
+from .models import news, articles
 
 News = news.News
-Articles = 
+Articles = articles.Articles
 
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
 
 #Getting the news base url
 base_url = app.config['NEWS_API_BASE_URL']
+#Getting the articles url
+articles_url = app.config['ARTICLES_BASE_URL']
 
 def get_news(category):
     '''
@@ -46,11 +48,11 @@ def process_results(news_list):
 
     return news_results  
 
-def get_articles(id):
+def get_articles(source_id, per_page):
     '''
     Function that gets articles 
     '''
-    get_articles_url = articles_url.format(id,api_key)
+    get_articles_url = articles_url.format(source_id,per_page,api_key)
 
     with urllib.request.urlopen(get_articles_url) as url:
         get_articles_data = url.read()
@@ -58,7 +60,29 @@ def get_articles(id):
 
         articles_results = None
 
-        if get_articles_response['articles']
+        if get_articles_response['articles']:
+            articles_results=process_articles(get_articles_response['articles'])
 
+    return articles_results
 
+def process_articles(all_articles):
+    '''
+    Function that processes the json result and transforms them to a list of objects 
+    '''
+    articles_list = []
+
+    for article in all_articles:
+        id = article.get('id')
+        name = article.get('name')
+        author = article.get('description')
+        title = article.get('title')
+        description = article.get('description')
+        url = article.get('url')        
+        urlToImage = article.get('urlToImage')
+        publishedAt = article.get('publishedAt')
+
+        if urlToImage:
+            article_object = Articles(id,name,author,title,description,url,urlToImage,publishedAt)
+            articles_list.append(article_object)
             
+    return articles_list
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 from app import app
-from .request import get_news
+from .request import get_news, get_articles
 
 #Views
 @app.route('/')
@@ -22,4 +22,14 @@ def index():
 
     title = 'Get the latest news in the World'
     return render_template('index.html', title = title, general = general_news, business = business_news, entertainment = entertainment_news, health = health_news, science = science_news, sports= sports_news,technology = technology_news )
+
+@app.route('/articles/<source_id>&<int:per_page>')
+def articles(source_id,per_page):
+    '''
+    Function that returns articles
+    '''
+
+    news_source = get_articles(source_id, per_page)
+    title = f'{source_id} | ARTICLES ' 
+    return render_template('articles.html', title= title, name=source_id, all_news = news_source)
 
